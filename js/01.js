9f2a382f8d212f7c139c1d0bd6173446ae1baeb3
Cam <camilajimenezr21@gmail.com>
2025-06-16 12:56:13 -0500

    Integración de notifación en formulario

--- js/01.js
+++ js/01.js
@@ -1,7 +1,30 @@
- const formulario = document.getElementById('formulario');
+const formulario = document.querySelector('#formulario');
 
-  formulario.addEventListener('submit', function () {
-    setTimeout(() => {
-      formulario.reset();
-    }, 300);
-  });
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (Notification.permission === 'granted') {
+        new Notification('Gracias, estaremos en contacto.', {
+            icon: '../img/1.jpg',
+            body: 'Tu mensaje fue enviado correctamente.'
+        });
+        setTimeout(() => {
+            formulario.submit();
+        }, 1000);
+    } else if (Notification.permission !== 'denied') {
+        Notification.requestPermission().then(permiso => {
+            if (permiso === 'granted') {
+                new Notification('Gracias, estaremos en contacto.', {
+                    icon: '../img/1.jpg',
+                    body: 'Tu mensaje fue enviado correctamente.'
+                });
+            }
+            setTimeout(() => {
+                formulario.submit();
+            }, 1000);
+        });
+    } else {
+        // permiso denegado, solo envía sin notificación
+        formulario.submit();
+    }
+});
